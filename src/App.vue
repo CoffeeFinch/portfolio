dc8537dc557a2c4dfaebbcd7ff38beaacc44b813
Will Gainsborough <wgainsborough37@gmail.com>
2023-07-02 19:28:51 -0400

    07.02.23

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,10 @@
 <template>
-  <div id="navbar">
-    <nav-bar></nav-bar>
-  </div>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
 </template>
-
-<script>
-import NavBar from './components/NavBar.vue';
-
-export default {
-  name: 'App',
-  components: {
-    NavBar
-  }
-}
-</script>
 
 <style>
 #app {
@@ -22,6 +13,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
